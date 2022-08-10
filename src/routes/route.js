c6f014f929d12816a06de196b83daa12cbe50bd0
Raunak Datar <raunakdatar22@gmail.com>
2022-08-10 16:16:32 +0530

    player

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,58 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+let players =
+[
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+]
+router.post("/players", function(req, res) {
+
+    let newplayer=req.body
+    for(let i=0;i<players.length;i++){
+        let newplayername=players[i]
+        if(newplayer.name==newplayername.name){
+            res.send(  "this player ahe adhich"  )
+           break; 
+        }
+    }
+players.push(newplayer)
+
+
+
+
+
+
+
+
+
+
+    res.send(  { data: players , status: true }  )
+})
+
 module.exports = router;
