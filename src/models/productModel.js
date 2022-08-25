5969910b5023e08483a66f9dfb35c47525446923
Raunak Datar <raunakdatar22@gmail.com>
2022-08-25 20:21:57 +0530

    middleware 2

---
+++ src/models/productModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+
+const productschema = new mongoose.Schema({
+
+    name:String,
+	category:String,
+	price:{
+        type:Number,
+        required:true
+    }
+}, {timestamps:true})
+
+module.exports=mongoose.model("product",productschema)
